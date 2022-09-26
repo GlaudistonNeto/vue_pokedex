7be88692402eea351a74b9245188e4a15fc7dc12
GlaudistonNeto <glaudistonson@gmail.com>
2022-09-26 13:06:06 -0300

    ✨feature: adding vue 3

---
+++ vue_3.0/vue_pokedex/src/components/CompPokemon.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemon.front" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ num }} - {{ upper(name) }}</p>
+            <p class="subtitle is-6">@{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  created: function() {
+    axios.get(this.url)
+         .then(res => {
+          this.pokemon.type = res.data.types[0].type.name;
+          this.pokemon.front = res.data.sprites.front_default;
+          this.pokemon.back = res.data.sprites.back_default;
+          console.log(this.pokemon);
+         })
+  },
+  data() {
+    return {
+      pokemon: {
+        type: '',
+        front: '',
+        back: ''
+      }
+    }
+  },
+  name: 'CompPokemon',
+  props: {
+    name: String,
+    url: String,
+    num: Number
+  },
+  filters: {
+    upper: function(value) {
+      var newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#pokemon {
+  margin-top: 2%;
+}
+</style>
